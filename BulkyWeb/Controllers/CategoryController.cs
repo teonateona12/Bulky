fbf79b810c60a9e294140abaa005c1feb8511d77
teonateona12 <teona.piranishvili1@gmail.com>
2024-01-28 21:54:57 -0800

    feat: add custom validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,6 +23,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exatly match the Name");
+            }
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
