93d481ffc5ab13b774d5116fc9b39f6d36d6f177
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-01 23:12:06 -0800

    feat: create product model

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        public double ListPrice { get; set; }
+        [Required]
+        [Display(Name = "List Price 1-50")]
+        public double Price { get; set; }
+        [Required]
+        [Display(Name = "List Price 50+")]
+        public double Price50 { get; set; }
+        [Required]
+        [Display(Name = "List Price 100+")]
+        public double Price100 { get; set; }
+    }
+}
